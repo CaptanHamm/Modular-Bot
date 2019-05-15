fabe73a43fea8667b249fe0153b4612a4bfafab6
Captain Hammer <36648742+CaptanHamm@users.noreply.github.com>
2019-05-15 16:57:22 -0500

    working jack command

--- commands/example.js
+++ commands/example.js
@@ -1,25 +1,36 @@
 module.exports = async ( client, message ) => {
-
+	function getRandomInt(max) {
+		return Math.floor(Math.random()*Math.floor(max));
+	};
+	let embed = {}
 	try {
-
-		let embed = {};
-
-		embed.title = "TITLE GOES HERE";
-
-		embed.description = '`------------------------------`\n';
-		embed.description += 'ENTER TEXT YOU WANT TO DISPLAY HERE \n';
-		embed.description += '`------------------------------`\n';
-		embed.description += "MORE TEXT HERE IF YOU WANT TO \n";
-		embed.description += '`------------------------------`\n\n';
-
-		embed.color = 0x2A6EBB;
-		embed.timestamp = new Date();
-
-		message.react('ℹ');
-		message.channel.send({embed});
-
-	} catch(e) {
-		throw e;
-	}
-
-}
+		let msg = "```md\n# Quote by Handsome Jack```";
+		let data = require('./audio.json');
+		let char = data.characters.find(t => t.name === 'Jack');
+		let audio = char.data.audio;
+		let number = getRandomInt(audio.length) - 1;
+		let play = audio[Math.max(0, number)];
+		//creates variable that plays randomized audio file
+		let isReady = true;
+		if (isReady) {
+		//sets bot to not being ready to accept new commands until audio file is finished playing.
+		isReady = false;
+		let voiceChannel = message.member.voiceChannel;
+		voiceChannel.join().then(connection => {
+		 //sets the local audio file location to play single specific item
+		 let dispatcher = connection.playFile(play);
+		 dispatcher.on("end", end => {
+			 //voiceChannel.leave();
+			 });
+		})
+		return message.channel.send(msg);
+	};
+} catch(e) {
+	 	console.log(e.message)
+	 	embed.color = 0x2ad68c;
+	 	embed.title = 'Whoopsie!';
+	 	embed.description = 'Wait, thats not right!';
+	 	embed.timestamp = new Date();
+	 	return message.channel.send({embed});
+	 }
+};
--- commands/jack.js
+++ commands/jack.js
@@ -1,30 +1,37 @@
-module.exports = async (client, message) => {
-  function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
-  let embed = {};
-  try {
-    let data = require('./audio.json');
-    let char = data.characters.find(t => t.name === 'Jack');
-    let audio = char.data.audio;
-    let number = getRandomInt(audio.length) - 1;
-    let quote = audio[Math.max(0, number)];
-    client.on('message', message => {
-    if (isReady && message.content === 'jack')
-    {
-    isReady = false;
-    var voiceChannel = message.member.voiceChannel;
-    voiceChannel.join().then(connection =>
-    {
-     const dispatcher = connection.playFile(random);
-     dispatcher.on("end", end => {
-    	 //voiceChannel.leave();
-    	 });
-    }).catch(err => console.log(err));
-    isReady = true;
-    }
-    });
-  } catch(e) {
-    console.log(e.message)
-  }
+module.exports = async ( client, message ) => {
+	function getRandomInt(max) {
+		return Math.floor(Math.random()*Math.floor(max));
+	};
+	let embed = {}
+	try {
+		let msg = "```md\n# Quote by Handsome Jack```";
+		let data = require('./audio.json');
+		let char = data.characters.find(t => t.name === 'Jack');
+		let audio = char.data.audio;
+		let number = getRandomInt(audio.length) - 1;
+		let play = audio[Math.max(0, number)];
+		//creates variable that plays randomized audio file
+		let isReady = true;
+		if (isReady) {
+		//sets bot to not being ready to accept new commands until audio file is finished playing.
+		isReady = false;
+		let voiceChannel = message.member.voiceChannel;
+		voiceChannel.join().then(connection => {
+		 //sets the local audio file location to play single specific item
+		 let dispatcher = connection.playFile(play);
+		 //let dispatcher = connection.playFile('C:/Github/Modular-Bot/Audio/Jack/bitch.ogg');
+		 dispatcher.on("end", end => {
+			 voiceChannel.leave();
+			 });
+		})
+		//return message.channel.send(msg);
+	};
+} catch(e) {
+	 	console.log(e.message)
+	 	embed.color = 0x2ad68c;
+	 	embed.title = 'Whoopsie!';
+	 	embed.description = 'Wait, thats not right!';
+	 	embed.timestamp = new Date();
+	 	return message.channel.send({embed});
+	 }
 };
